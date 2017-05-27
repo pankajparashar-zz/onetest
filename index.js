69deee2cb9b2e5ea9b8a49491f20c27cbcd55285
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2017-05-27 22:35:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+class OneTest {
+
+    constructor () {
+        process.env.PATH = process.env.PATH + ':/Users/pankajparashar/Github/onetest/drivers/'
+        this.webdriver = require('selenium-webdriver');        
+        this.driver = new this.webdriver.Builder().
+                          withCapabilities(this.webdriver.Capabilities.chrome()).
+                          build();
+    }
+
+    method1 () {
+        this.driver.get('http://www.google.com');
+        this.driver.findElement(this.webdriver.By.name('q')).sendKeys('simple programmer');
+        this.driver.findElement(this.webdriver.By.name('btnG')).click();
+        this.driver.quit();
+    }
+}
+
+let test = new OneTest()
+test.method1()
